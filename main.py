ac76ddf4a907efe8cfe53e79ef409d4491118d7d
kshnpch-cmyk <kshnpch@gmail.com>
2026-09-23 14:20:23 +0900

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ warnings.filterwarnings('ignore')
 
 # 💡 최신 배포 웹앱 URL 지정
 LOGIN_URL = "https://admin.theborn.co.kr/oms-manager/login"
-WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyabKFCct7iGmBY5y29--cHmZCtryW4dSm0zSzFEV5nOxQ-wwsAvFjRkCEPyTHnVTfX/exec"
+WEBAPP_URL = "https://script.google.com/macros/s/AKfycbyNkVPO--OF2zqbSSIlO_8c1erkOqvaMqA2pLJaP2OpJjGZyRjMjy27X9b7zXgwe_kM/exec"
 
 OMS_COMPANY_CODE = os.environ.get("COMPANY_CD", "1000").strip()
 OMS_ID = os.environ.get("USER_ID", "").strip()
